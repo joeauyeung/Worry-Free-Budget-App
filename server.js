c4bb63fc178aaa9f48f9a0f4e47b39f16468bceb
Joe Au-Yeung <j.auyeung419@gmail.com>
2021-02-25 19:43:21 -0500

    Start Passport Google Auth

--- server.js
+++ server.js
@@ -1,16 +1,32 @@
 const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
+require('dotenv').config();
+const GoogleStrategy = require("passport-google-oauth20");
+const passport = require("passport");
 
-
+// Setup app
 app = express();
 app.use(cors());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
+// Use CSS style sheet
+app.use(express.static(__dirname + "/client/css"))
+
+// Setting up authentication through Google
+
+passport.use(new GoogleStrategy({
+    clientID: process.env.GOOGLE_ID,
+    clientSecret: process.env.GOOGLE_SECRET,
+    callbackURL: "http://localhost300/auth/google/callback"
+},
+    function(accessToken, refreshToken, profile,cb)
+))
+
 app.route("/")
     .get((req, res) => {
-        res.sendFile("C:\Users\Joe Au-Yeung\Documents\Webdev\Budget_App\Staging\client\public\login.html")
+        res.sendFile(__dirname +  "/client/static-pages/login.html")
     });
 
 
