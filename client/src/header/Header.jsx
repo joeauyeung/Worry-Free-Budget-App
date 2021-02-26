16b2846f365fb5bc077ec8e2f30c2983288995d1
Joe Au-Yeung <j.auyeung419@gmail.com>
2021-02-26 17:20:26 -0500

    Start using useStyle mui

--- client/src/header/Header.jsx
+++ client/src/header/Header.jsx
@@ -4,16 +4,25 @@ import Toolbar from "@material-ui/core/Toolbar";
 import IconButton from "@material-ui/core/IconButton";
 import MenuIcon from "@material-ui/icons/Menu";
 import Typography from "@material-ui/core/Typography"
+import {makeStyles} from "@material-ui/styles"
+
+const useStyles = makeStyles({
+    header: {
+        paddingLeft: "1%",
+        fontSize: "1.2vw"
+    }
+})
 
 
 function Header() {
+    const classes = useStyles();
     return <div>
         <AppBar>
             <Toolbar>
                 <IconButton>
                     <MenuIcon/>
                 </IconButton>
-                <Typography varient="h3">
+                <Typography className={classes.header}>
                     Worry Free Budget
                 </Typography>
             </Toolbar>
--- client/src/login/Login.jsx
+++ client/src/login/Login.jsx
@@ -1,11 +1,20 @@
 import React from "react";
 import Button from "@material-ui/core/Button";
+import {makeStyles} from "@material-ui/styles"
 
+const useStyles = makeStyles({
+    title: {
+        textAlign: "center",
+        fontSize: "3vw",
+        paddingTop: "5%"
+    }
+})
 
 function Login() {
+    const classes = useStyles();
     return(
         <div>
-        <h1>Worry Free Money Budget Tracker</h1>
+        <h1 className={classes.title}>Worry Free Money Budget Tracker</h1>
         <h3>Based on the book "Worry-Free Money: The Guilt-free Approach to Managing Your Money and Your Life"
         by Shannon Lee Simmons</h3>
         <Button variant="contained" color="secondary"> Login / signup with Google </Button>
